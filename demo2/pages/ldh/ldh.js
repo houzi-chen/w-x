de05c2eae58bf0088609f3c963deaed995cd15f8
zelin <zelin@zelindeMacBook-Pro-47.local>
2020-05-18 19:59:09 +0800

    demo2

---
+++ demo2/pages/ldh/ldh.js
@@ -0,0 +1,76 @@
+// pages/ldh/ldh.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    chinese: "刘德华",
+    foregin: "Andy Lau, Lau Tak Wah",
+    alias: "华仔",
+    nationality: "中国",
+    nation: "汉族",
+    xz: "天秤座",
+    blood: "AB型",
+    tall: "174cm",
+    weight: "63kg",
+    adress: "香港新界大埔镇泰亨村",
+    date: "1961年9月27日"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
